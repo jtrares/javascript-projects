fa632ac089bfa3ac1bbed510d4de2a14e843eb8a
jtrares <jasminetrares@gmail.com>
2024-01-07 19:11:36 -0600

    Fixed statements

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -7,6 +7,7 @@ const distToMoonKM = 384400
 const milePerKm = 0.621
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+
 console.log(typeof shuttleName)
 console.log(typeof shuttleSpeedMph)
 console.log(typeof distToMarsKm)
@@ -18,14 +19,17 @@ console.log(typeof milePerKm)
 let milesToMars = (distToMarsKm * milePerKm)
 let hrsToMars = (milesToMars / shuttleSpeedMph)
 let daysToMars = (hrsToMars / 24)
-console.log(shuttleName + ' will take ' + daysToMars + ' days to reach Mars.')
- 
+
 // Print the results of the space mission calculations below
 
+console.log(shuttleName + ' will take ' + daysToMars + ' days to reach Mars.')
+
 // Calculate a trip to the moon below
+
 let milesToMoon = (distToMoonKM * milePerKm)
 let hrsToMoon = (milesToMoon / shuttleSpeedMph)
 let dayToMoon = (hrsToMoon / 24)
-console.log(shuttleName + ' will take ' + dayToMoon + ' days to reach the Moon')
 
 // Print the results of the trip to the moon below
+
+console.log(shuttleName + ' will take ' + dayToMoon + ' days to reach the Moon')
