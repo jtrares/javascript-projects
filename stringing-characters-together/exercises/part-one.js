7544fa6481bb8528afb7015f10678e139e95e472
jtrares <jasminetrares@gmail.com>
2024-01-15 18:33:41 -0600

    Exercises:Strings

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,28 @@
 let num = 1001;
-
+ 
 //Returns 'undefined'.
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+str = num.toString();
+len = str.length; 
+console.log(len)
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
+//Modify your code to print out the length of a decimal value EXCLUDING the period.
+//var value = 192.123123; stringValue = value.toString(); length = stringValue.split('.')[1].length;
+num = 123.45;
+str = num.toString();
+let len = str.length
+if (str.includes('.')) {
+    len = str.length -1
+}
+console.log(len)
 
-//Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+//Experiment! What if num could be EITHER an integer or a decimal?  
+//Add an if/else statement so your code can handle both cases.
+if (String(num).includes('.')) {
+    console.log(String(num).length - 1);
+} else {
+    console.log(String(num).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -1,17 +1,25 @@
 //Part Three section one
-
-let language = 'JavaScript';
+const language = "JavaScript";
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
+let str = language.slice(0, 1);
+let strTwo = language.slice(4, 5);
+console.log(str + strTwo);
 
 //2. Without using slice(), use method chaining to accomplish the same thing.
+language.split("").toSpliced(1, 3).toSpliced(2, 5);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
+let initials = "JS";
+console.log(`The abrreviation for '${language}' is '${initials}'.`);
 
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+let fun = "Tacos are life!"
+let newFun = fun.repeat(4).replace('are', 'equal').repeat(3);
+console.log(newFun);
 
 //Part Three section Two
-
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
-
-let notTitleCase = 'title case';
+let notTitleCase = "title case";
+let titleCase = notTitleCase.replace('t', 'T').replace('c', 'C')
+console.log(titleCase);
